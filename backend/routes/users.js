b21a638c0e632388f1e5d9ec964b1453f312c1f7
Yashua Saguin <yashuasaguin@gmail.com>
2023-02-22 16:41:51 -0600

    Added hashing to password using bcrypt

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-
+const bcrycpt = require('bcrypt')
 const org = process.env.ORG
 
 const { users } = require('../models/models')
 
+
+// Login api endpoint
 router.post('/', async (req, res, next) => {
     const {username, password} = req.body
 
@@ -17,6 +19,9 @@ router.post('/', async (req, res, next) => {
         if (!user) {
             return res.status(400).json({error: "Invalid login"});
         }
+        if (!await bcrycpt.compare(password, user.password)) {
+            return res.status(400).json({error: "Invalid login"});
+        }
 
         res.json(user);
     } catch (err) {
@@ -26,4 +31,20 @@ router.post('/', async (req, res, next) => {
    
 })
 
+
+//Create user API endpoint
+router.post('/register', async (req, res) => {
+    const userData = {
+        username: req.body.username,
+        role: req.body.role,
+        orgs: org
+    }
+
+    if(req.body.password != null || req.body.password != "") {
+        userData.password = await bcrycpt.hash(req.body.password, 10)
+    }
+
+    const user = users.create(userData);
+    console.log('Success')
+})
 module.exports = router
