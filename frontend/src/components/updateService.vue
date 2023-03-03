a8c93b55ad01c6a924fa26edac354b9cf1f70287
YashuaIT <ysaguin@cougarnet.uh.edu>
2023-03-03 02:13:20 -0600

    Changed input type for services status property

--- frontend/src/components/updateService.vue
+++ frontend/src/components/updateService.vue
@@ -78,14 +78,16 @@ export default {
                         <label class="block">
                             <span class="text-gray-700">Status</span>
                             <span style="color: #ff0000">*</span>
-                            <input type="text"
-                                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                v-model="services.status" />
-                            <span class="text-black" v-if="v$.services.status.$error">
-                                <p class="text-red-700" v-for="error of v$.services.status.$errors" :key="error.$uid">
-                                    {{ error.$message }}!
-                                </p>
-                            </span>
+                            <div>
+                                <input type="radio" id="active" name="status" value="active" v-model="services.status"
+                                    class="w-4 h-4  focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50" />
+                                <label for="active" class="ml-2">Active</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="inactive" name="status" value="inactive" v-model="services.status"
+                                    class="w-4 h-4  focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50" />
+                                <label for="active" class="ml-2">Inactive</label>
+                            </div>
                         </label>
                     </div>
 
